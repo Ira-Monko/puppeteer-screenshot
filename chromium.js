903008a979bf2646aa890346aee46a8ebeff4782
Ira Monko <ira.monko@gmail.com>
2019-05-12 17:45:53 -0400

    Updated chromium.js

--- chromium.js
+++ chromium.js
@@ -2,17 +2,19 @@ const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 async function getScreenshot(url, type, quality, fullPage) {
-    const browser = await puppeteer.launch({
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-    });
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  });
 
-    const page = await browser.newPage();
-    await page.goto(url);
-    const file = await page.screenshot({ type,  quality, fullPage });
-    await browser.close();
-    return file;
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.evaluate(() => (document.body.style.background = 'transparent'));
+  await page.waitFor(1500);
+  const file = await page.screenshot({ type, quality, fullPage });
+  await browser.close();
+  return file;
 }
 
 module.exports = { getScreenshot };
